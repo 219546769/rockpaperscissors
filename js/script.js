42409d35b3f8d52a8417bae1e700ae413bc69da0
Alan Perez <alanperez@whsat.org>
2018-11-27 21:59:51 +0000

    finished i guess

--- js/script.js
+++ js/script.js
@@ -4,10 +4,38 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-$("button").click(function(){
-    $("#userChoice").text($("#input").val());
+$("button").click(function() {
+    $("#userChoice").text($("#input")).val();
+
+
+    var computerChoice = Math.random();
+    console.log(computerChoice);
+    var computerChoiceName;
+    var userChoice="";
+    var winner = "";
+   
+   
+    if(computerChoice > .00 && computerChoice < 0.3) {
+        $("#computerChoice").text("Paper!"); 
+        computerChoiceName = "paper";
+    }
+    else if(computerChoice > .30 && computerChoice < 0.6) {
+        $("#computerChoice").text("Rock!"); 
+         computerChoiceName = "rock";
+    }
+    else if(computerChoice > .60 && computerChoice < 1.0) {
+        $("#computerChoice").text("Scissor!");
+         computerChoiceName = "scissor";
+    }
+   
+   console.log (computerChoiceName);
+   console.log (computerChoice);
+   
+    $("#computerChoice").text(computerChoiceName); 
+     
+
 });
+   
 
 
 // DOCUMENT READY FUNCTION BELOW
-alert("hi");
